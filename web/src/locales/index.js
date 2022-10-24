1df2e388a0a4384932ca99ea6a0b1ab6c6599675
Thanh Nguyen <nvthanh1804@gmail.com>
2022-10-24 14:54:24 +0900

    change locales json to object

---
+++ web/src/locales/index.js
@@ -0,0 +1,14 @@
+export const messages = {
+    en: {
+        hello: "Hello!",
+        login: "Log In",
+    },
+    vi: {
+        hello: "Xin chào!",
+        login: "Đăng nhập",
+    },
+    ja: {
+        hello: "こんにちは、世界",
+        login: "ログイン",
+    },
+};

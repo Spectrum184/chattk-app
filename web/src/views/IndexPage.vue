95902003ba6415c5c625e26c97b891dc08b22960
Thanh Nguyen <nvthanh1804@gmail.com>
2022-10-28 19:17:34 +0900

    fix css

--- web/src/views/IndexPage.vue
+++ web/src/views/IndexPage.vue
@@ -1,14 +1,19 @@
 <template>
     <LoginPage v-if="!userStore.getUser" />
-    <template v-else>
-        <h1>Click on the hamburger icon and open a chat.</h1>
-    </template>
+    <div v-else class="w-full flex items-center justify-center">
+        <h1 class="text-black">{{ t("homePage.mainMessage") }}</h1>
+    </div>
 </template>
 <script setup>
 import { useUserStore } from "@/stores/user";
 import LoginPage from "@/views/LoginPage.vue";
 import { useTitle } from "@vueuse/core";
 import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n({
+    inheritLocale: true,
+});
 
 const userStore = useUserStore();
 useTitle(computed(() => (userStore.getUser ? "Home" : "Log In")));

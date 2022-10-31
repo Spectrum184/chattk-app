2ef0ce818dc2f91e0312490fdd46f370974ef898
Thanh Nguyen <nvthanh1804@gmail.com>
2022-10-31 19:04:58 +0900

    add user ingo

---
+++ api/src/models/user-info.model.ts
@@ -0,0 +1,32 @@
+export enum SexEnum {
+    Male = "Male",
+    Female = "Female",
+    Other = "Other",
+}
+export interface UserInfo {
+    id: string;
+    firstName: string;
+    lastName: string;
+    userId: string;
+    phone: string;
+    facebook: string;
+    twitter: string;
+    birthday: Date;
+    updatedAt: Date;
+}
+
+export interface UserInfoDoc extends Omit<UserInfo, "id"> {
+    _id: string;
+}
+
+export const userInfoProjection = {
+    id: 1,
+    firstName: 1,
+    lastName: 1,
+    userId: 1,
+    phone: 1,
+    facebook: 1,
+    twitter: 1,
+    birthday: 1,
+    updatedAt: 1,
+};
